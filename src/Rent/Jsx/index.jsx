7cc0adca3f18171c556248619078caef86071af8
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2023-10-23 18:38:16 +0100

    implement complete server req

--- src/Rent/Jsx/index.jsx
+++ src/Rent/Jsx/index.jsx
@@ -1,13 +1,22 @@
 import {useState,useEffect} from 'react'
 import { SearchComponent, Brands,Cars} from "./components";
+import { useQueryClient } from '@tanstack/react-query';
 
 
 export default function Rent(){
         const [brand,setBrand] = useState('');
-
+        const queryClient  =  useQueryClient();
+console.log("render or rerender");
         useEffect(function(){
                 console.log(`the current brand is :${brand}`);
         });
+
+        // useEffect(function(){
+        //         console.log("inside useEffect invalidator");
+        //         // queryClient.removeQueries();
+        //         // queryClient.refetchQueries();
+
+        // }),[brand];
 
     return <div id='rentContainer'>
                 <SearchComponent/>
