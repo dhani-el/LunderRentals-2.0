681dc1a7a1761b3ecca3a0dc101c45c381f61e4c
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-03-13 17:17:02 +0100

    re implement footer for landscape

--- src/Home/Jsx/index.jsx
+++ src/Home/Jsx/index.jsx
@@ -29,7 +29,7 @@ export default function Home(){
                     </div>
                     <CallToAction/>
                 </div>
-                <Modelo setModelReady={setRemoveSplash}/>
+                {/* <Modelo setModelReady={setRemoveSplash}/> */}
                 <BodyContent/>
             </div>
             </div>
