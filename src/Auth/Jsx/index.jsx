63ce175c71ec172761465374a941a4b4894e5651
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-06-14 16:18:29 +0100

    implement login and signup feedback and redirect

--- src/Auth/Jsx/index.jsx
+++ src/Auth/Jsx/index.jsx
@@ -7,10 +7,12 @@ import '../Styles/index.css';
 export default function Auth(){
 
     const [form , setForm] = useState(formType.Login);
-
+    function nav(){
+        setForm(init=> formType.Login)
+    }
     return <div id="authPageContainer">
         <AuthSelector setFormFunction={setForm} form={form} />
-        { (form == formType.Login) && <Login/>}
-        { (form == formType.SIGNUP) &&  <SignUp/>}
+        { (form == formType.Login) && <Login  />}
+        { (form == formType.SIGNUP) &&  <SignUp nav={nav}/>}
     </div>
 }
